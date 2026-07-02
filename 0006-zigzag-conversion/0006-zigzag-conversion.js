d35325433f526d218bbf295885cffa4f6992f753
Priyanshu <193862903+Priyansh7u@users.noreply.github.com>
2026-07-02 15:29:53 +0530

    Time: 3 ms (89.8%), Space: 57.8 MB (70.17%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || numRows >= s.length) {
+        return s;
+    }
+
+    const rows = new Array(numRows).fill("");
+
+    let currentRow = 0;
+    let goingDown = true;
+
+    for (const char of s) {
+        rows[currentRow] += char;
+
+        if (currentRow === 0) {
+            goingDown = true;
+        } else if (currentRow === numRows - 1) {
+            goingDown = false;
+        }
+
+        currentRow += goingDown ? 1 : -1;
+    }
+
+    return rows.join("");
+};
