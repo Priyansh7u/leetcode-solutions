0029e381a2e59d3addb11cc8feb7a83d14796344
Priyanshu <193862903+Priyansh7u@users.noreply.github.com>
2026-07-02 16:28:22 +0530

    Time: 194 ms (54.05%), Space: 79.6 MB (72.59%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.js
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.js
@@ -2,7 +2,11 @@ var maxSum = function(nums, k, mul) {
     nums.sort((a, b) => b - a);
     let sum = 0;
     for (let i = 0; i < k; i++) {
-        sum += nums[i] * Math.max(1, mul - i);
+        if (mul - i > 1) {
+            sum += nums[i] * (mul - i);
+        } else {
+            sum += nums[i];
+        }
     }
     return sum;
 };
